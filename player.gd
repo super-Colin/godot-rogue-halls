98ae9c37c990657c3882424ac881ac962e3089d1
unknown <supercolin.dev@gmail.com>
2025-08-24 09:18:44 -0400

    adjusted player movement and some organizing

--- player.gd
+++ player.gd
@@ -5,26 +5,20 @@ extends CharacterBody2D
 const weapon = preload("res://gun.tscn")
 
 
-
+#region Exports
 @export_group("Movement")
 ## Maximum speed reachable by player
-@export_range(0, 500) var max_speed := 300.0
+@export_range(0, 500) var max_speed := 400.0
+@export_range(0, 1000) var max_fall_speed := 1000.0
 @export_range(0, 500) var max_speed_influence := 200.0
 ## Acceleration while on the ground (how quickly the player reaches max speed)
 @export_range(0, 500) var acceleration := 100.0
-## Friction while on group (how quickly the player slows down)
+## Friction while on ground (how quickly the player slows down)
 @export_range(0, 50) var friction := 50.0
 ## Acceleration while in the air (how quickly the player reaches max speed)
 @export_range(0, 500) var air_acceleration := 5.0
 ## Air friction while in the air (how quickly the player slows down)
-@export_range(0, 50) var air_resistance := 50.0
-
-
-@export_group("Jump Assist")
-## Max amount of time allowed after leaving the ground while still being able to jump
-@export_range(0, 1) var coyote_timer_value = 0.1
-## Max amount of time the game holds on to the players input to accecute when avaiable
-@export_range(0, 1) var jump_buffer_timer_value = 0.15
+@export_range(0, 50) var air_resistance := 35.0
 
 
 @export_group("Jump")
@@ -50,6 +44,7 @@ const weapon = preload("res://gun.tscn")
 # ----
 @export var handReach = 60
 @export var handAngularSpeed: float = 10.0  # Radians per second
+#endregion Exports
 
 var dead = false
 var currentInteractionObject:Node
@@ -59,8 +54,6 @@ var handFacing
 
 var weaponGenerationOn = true
 
-
-var responseToAnimationFinished
 
 signal updateInteractionPrompt(string)
 
@@ -82,31 +75,13 @@ func _ready():
 	%PlayerSprite.sprite_frames.set_animation_loop("Death", false)
 	setupHeldItem()
 	Globals.s_playerReady.emit()
+	print("player - ready")
 
-func setupHeldItem():
-	var heldItem = $Hand.getHeldItem()
-	if heldItem:
-		if "setupWeapon" in heldItem:
-			heldItem.setupWeapon(Stats.weapon.maxBonusPercent, Stats.weapon.maxBonusFlat, Stats.weapon.generationBonusPercent)
-	
 
 func _process(delta: float) -> void:
 	if dead:
 		return
 	trackMouseWithHandDelta(delta)
-
-
-
-
-
-
-
-
-
-# Sets the gravity depending on the context
-#func _get_gravity(_velocity):
-	#return jump_gravity if _velocity.y < 0.0 else fall_gravity
-
 
 
 func _physics_process(delta):
@@ -115,32 +90,17 @@ func _physics_process(delta):
 	_set_player_direction(sign($Hand.position.x)) # hand is tracking mouse in _process()
 	# Even if the player cannot control the character, apply gravity
 	if not onFloor:
-		velocity += get_gravity() * delta
+		apply_gravity(delta)
 	#if dead or not Globals.playerIsControllable:
 	if not dead and Globals.playerIsControllable:
-		if not onFloor:
+		if not onFloor: # in the air
 			_get_movement(air_resistance, air_acceleration, delta)
-		else:
+		else: # on the ground
 			_get_movement(friction, acceleration, delta)
-		# fire laser / jump etc..
-		handleInputs(onFloor, facingBackward)
-		# Charge oxgen / laser 
-		setSpriteAnimation(onFloor, facingBackward)
-		handleRecharges(delta)
+		handleInputs(onFloor, facingBackward) # Actions- fire laser / jump etc..
+		setSpriteAnimation(onFloor, facingBackward) # Set sprite state
+		handleRecharges(delta) # Charge energy / oxgen / laser ..
 	move_and_slide()
-
-func handleRecharges(delta):
-	if currentEnvironmentRef:
-		if currentEnvironmentRef.infiniteEnergy:
-			fullRecharge()
-	else:
-		# Oxygen / Health / Energy
-		suitGeneration(delta)
-		# External Generations / Drains
-		weaponGeneration(delta)
-		#equipmentGeneration(delta)
-		# Environmental Generations / Drains
-		handleOxygenDrain(delta)
 
 func handleInputs(onFloor, facingBackward=false):
 	#if Input.is_action_just_pressed("Jump"):
@@ -154,13 +114,42 @@ func handleInputs(onFloor, facingBackward=false):
 	elif Input.is_action_just_released("Jump"):
 		jump_cut()
 	if Input.is_action_just_pressed("Fire"):
-		fireLaser()
+		fireWeapon()
 	if Input.is_action_just_released("Interact"):
 		if currentInteractionObject != null:
 			#print("player - interacting with object: ", currentInteractionObject)
 			currentInteractionObject.playerInteraction()
 
 
+#region Velocity / Movement
+func apply_gravity(delta):
+	var gravVector = get_gravity()
+	velocity += gravVector * delta
+	#if velocity.y > max_speed: # if falling too fast
+	if velocity.y > max_fall_speed: # if falling too fast
+		velocity.y = move_toward(velocity.y, max_fall_speed, air_resistance * delta * 100)
+	elif velocity.y < -max_speed:
+		velocity.y = move_toward(velocity.y, -max_speed, air_resistance * delta * 100)
+	#return jump_gravity if _velocity.y < 0.0 else fall_gravity
+
+# Calculates the players movement depending on the context
+func _get_movement(fric: float, accel: float, delta: float):
+	var direction = Input.get_axis("Move_Left", "Move_Right")
+	var walkingInfluence
+	if direction:
+		walkingInfluence = sign(direction) * accel * delta * 100
+		# if not at max speed 
+		# if sign(direction) != sign(velocity.x) or (not velocity.x < -max_speed and not velocity.x > max_speed):
+		if (not velocity.x < -max_speed and not velocity.x > max_speed):
+			velocity.x += walkingInfluence
+		else:
+			velocity.x = move_toward(velocity.x, max_speed, fric * delta * 100)
+	else:
+		velocity.x = move_toward(velocity.x, 0, fric * delta * 100)
+#endregion Velocity / Movement
+
+
+#region Animation
 func setSpriteAnimation(onFloor, facingBackward=false):
 	if onFloor:
 		if velocity.x != 0:
@@ -176,83 +165,48 @@ func setSpriteAnimation(onFloor, facingBackward=false):
 		else:
 			%PlayerSprite.play("Jetpack")
 
+func checkIfAnimationShouldStop():
+	print("player - checking if dead: ", %PlayerSprite.animation)
+	if %PlayerSprite.animation == "Dieing":
+		print("player - player is dead: ")
+		%PlayerSprite.stop()
 
-# Calculates the players movement depending on the context
-func _get_movement(fric: float, accel: float, delta: float):
-	var direction = Input.get_axis("Move_Left", "Move_Right")
-	var walkingInfluence
-	if direction:
-		walkingInfluence = sign(direction) * accel * delta * 100
-		# if not at max speed 
-		if sign(direction) != sign(velocity.x) or (not velocity.x < -max_speed and not velocity.x > max_speed):
-			velocity.x += walkingInfluence 
-		else:
-			velocity.x = move_toward(velocity.x, max_speed, fric * delta * 100)
+func flipDirection():
+	print("player - flipping direction")
+	if %PlayerSprite.flip_h:
+		_set_player_direction(1)
 	else:
-		velocity.x = move_toward(velocity.x, 0, fric * delta * 100)
+		_set_player_direction(-1)
+
+func _set_player_direction(direction: int) -> void:
+	#print("player - flashlight direction is: ", direction)
+	if direction > 0.0: # moving right
+		%Flashlight.rotation = 0
+		%PlayerSprite.flip_h = false
+	if direction < 0.0: # moving left
+		%Flashlight.rotation = PI
+		%PlayerSprite.flip_h = true
+#endregion Animation
 
 
-
-
-func useJetpack():
-	print("player - using jet pack")
-
-
-
-
-
-
-
-
-
-
-
-
+#region Charging / Draining
+func handleRecharges(delta):
+	if currentEnvironmentRef:
+		if currentEnvironmentRef.infiniteEnergy:
+			#print("player - inf energy")
+			fullRecharge(delta)
+	else:
+		# Oxygen / Health / Energy
+		suitGeneration(delta)
+		# External Generations / Drains
+		weaponGeneration(delta)
+		#equipmentGeneration(delta)
+		# Environmental Generations / Drains
+		handleOxygenDrain(delta)
 
 func suitGeneration(delta):
 	generateEnergy(delta)
 	generateOxygen(delta)
-
-func weaponGeneration(delta):
-	var energyCost = $Hand.getHeldItem().getGenerationEnergyCost(delta)
-	if canAffordEnergyCost(energyCost):
-		drainEnergy(energyCost)
-		$Hand.getHeldItem().generateAmmo(delta)
-		#generateWeapon(delta)
-
-func canAffordEnergyCost(cost)->bool:
-	return Stats.energy.current > cost
-
-
-
-func generateWeapon(delta):
-	#if not Stats.laserRegenerationOn:
-		#return
-	var energyCost = delta * Stats.weapon.generationEfficiency
-	#var unitProduction = energy * laserGenerationRate
-	if Stats.laserCharges < Stats.laserChargesMax and Stats.energy > energyCost:
-		Stats.energy -= energyCost
-		Stats.laserCharges += delta * Stats.laserGenerationRate
-
-
-
-func generateOxygen(delta):
-	var energyCost = (Stats.oxygen.generationRate * delta) * Stats.oxygen.generationEfficiency
-	if Stats.energy.current >= energyCost and Stats.oxygen.current < Stats.functionalMax("oxygen"):
-		Stats.energy.current -= energyCost 
-		Stats.oxygen.current += delta * Stats.oxygen.generationRate 
-
-func fullRecharge():
-	#Stats.laserCharges = Stats.laserChargesMax
-	Stats.energy.current = Stats.energy.max
-	Stats.oxygen.current = Stats.oxygen.max
-
-func handleOxygenDrain(delta):
-	Stats.oxygen.current -= delta
-	if Stats.oxygen.current < -4.0: # a few seconds of red screen
-		playerDied()
-		#print("player - died from oxygen")
-
 
 func generateEnergy(delta):
 	var amount = Stats.energy.generationRate * delta
@@ -264,28 +218,39 @@ func drainEnergy(amount):
 	if Stats.energy.current == 0:
 		print("player - out of energy, can't drain...")
 
+func generateOxygen(delta):
+	var energyCost = (Stats.oxygen.generationRate * delta) * Stats.oxygen.generationEfficiency
+	if Stats.energy.current >= energyCost and Stats.oxygen.current < Stats.functionalMax("oxygen"):
+		Stats.energy.current -= energyCost 
+		Stats.oxygen.current += delta * Stats.oxygen.generationRate 
+
+func handleOxygenDrain(delta):
+	Stats.oxygen.current -= delta
+	if Stats.oxygen.current < -4.0: # a few seconds of red screen
+		playerDied()
+		#print("player - died from oxygen")
+
+func weaponGeneration(delta, effiency=1.0): # effiency: lower is better
+	#var energyCost = $Hand.getHeldItem().getGenerationEnergyCost(delta) * effiency
+	var energyCost = $Hand.getHeldItem().getGenerationEnergyCost(delta * Stats.weapon.generationRate) * effiency
+	if canAffordEnergyCost(energyCost):
+		drainEnergy(energyCost)
+		$Hand.getHeldItem().generateAmmo(delta * Stats.weapon.generationRate)
+		#generateWeapon(delta)
+
+func canAffordEnergyCost(cost)->bool:
+	return Stats.energy.current > cost
+
+func fullRecharge(delta=0):
+	if delta == 0:
+		delta = 10000
+	Stats.energy.current = Stats.energy.max
+	Stats.oxygen.current = Stats.oxygen.max
+	weaponGeneration(delta, 0.0)
+#endregion Charging / Draining
 
 
-
-func fireLaser():
-	if "fireWeapon" in $Hand.heldObject:
-		if $Hand.heldObject.fireWeapon():
-			var recoilImpulse = Vector2(-2000,0).rotated($Hand.heldObject.global_rotation)
-			print("player - laser recoil impulse: ", recoilImpulse)
-			$'.'.velocity += recoilImpulse
-
-
-
-
-
-
-
-
-
-
-
-
-
+#region Prompt
 func checkForPromptUpdate(areaOrBody:Node, entered:bool): # entered or exited
 	if not "playerInteraction" in areaOrBody:
 		return
@@ -304,27 +269,13 @@ func clearInteractionOject():
 func updatePrompt(newPrompt):
 	%InteractionPrompt.text = newPrompt
 	%InteractionPrompt.visible = true
+#endregion Prompt
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#region Jumping
 # Adds the player's jump velocity if able
 func jump():
 	velocity.y = jump_velocity
-	#%PlayerSprite.play("Jetpack")
-	#responseToAnimationFinished = ""
-
 
 # Stops jump acceleration if variable_jump_height is enabled
 func jump_cut():
@@ -333,34 +284,17 @@ func jump_cut():
 	if velocity.y < minimum_jump_height * up_direction.y:
 		velocity.y = minimum_jump_height * up_direction.y
 
+func useJetpack():
+	print("player - using jet pack")
+#endregion Jumping
 
 
-func flipDirection():
-	print("player - flipping direction")
-	if %PlayerSprite.flip_h:
-		_set_player_direction(1)
-	else:
-		_set_player_direction(-1)
-
-func _set_player_direction(direction: int) -> void:
-	#return
-	#print("player - flashlight direction is: ", direction)
-	if direction > 0.0: # moving right
-		%Flashlight.rotation = 0
-		#%PlayerSprite.flip_h = false
-		%PlayerSprite.flip_h = false
-	if direction < 0.0: # moving left
-		%Flashlight.rotation = PI
-		#%PlayerSprite.flip_h = true
-		%PlayerSprite.flip_h = true
-
-
-
-
-
-
-#func trackMouseWithHand():
-	#$Hand.position = handReach * get_local_mouse_position().normalized()
+#region Hand Item
+func setupHeldItem():
+	var heldItem = $Hand.getHeldItem()
+	if heldItem:
+		if "setupWeapon" in heldItem:
+			heldItem.setupWeapon(Stats.weapon.maxBonusPercent, Stats.weapon.maxBonusFlat, Stats.weapon.generationBonusPercent)
 
 func trackMouseWithHandDelta(delta):
 	var origin = Vector2.ZERO  # assuming local space origin
@@ -372,6 +306,7 @@ func trackMouseWithHandDelta(delta):
 	# Move the current angle toward the target angle
 	var new_angle = move_toward_angle(current_angle, target_angle, handAngularSpeed * delta)
 	# Update hand position
+	#$Hand.position = handReach * get_local_mouse_position().normalized()
 	$Hand.position = Vector2(handReach, 0).rotated(new_angle)
 	# Optionally rotate the hand to point outward
 	$Hand.rotation = new_angle
@@ -383,12 +318,28 @@ func move_toward_angle(from_angle: float, to_angle: float, max_step: float) -> f
 		return to_angle
 	return from_angle + sign(delta) * max_step
 
+func fireWeapon():
+	if "fireWeapon" in $Hand.heldObject:
+		if $Hand.heldObject.fireWeapon():
+			var recoilImpulse = Vector2(-2000,0).rotated($Hand.heldObject.global_rotation)
+			#print("player - laser recoil impulse: ", recoilImpulse)
+			$'.'.velocity += recoilImpulse
+
+#endregion Hand Item
 
 
+#region Environment
+func enteredEnvironment(newEnvironment):
+	if newEnvironment is EnvironmentSettings:
+		currentEnvironmentRef = newEnvironment
+		#print("player - entered newEnvironment: ", newEnvironment)
 
+func exitedEnvironment(environment):
+	if environment == currentEnvironmentRef:
+		currentEnvironmentRef == null
+		#print("player - exited the environment: ", environment)
 
-
-
+#endregion Environment
 
 
 
@@ -404,40 +355,12 @@ func turnLightsOn():
 
 
 
-
-
-
 func playerDied():
 	Globals.s_playerDied.emit()
 	dead = true
 	%PlayerSprite.modulate = "aa5853"
 	%PlayerSprite.play("Death")
 	$Hand.dropHeldItem()
-
-
-
-
-func enteredEnvironment(newEnvironment):
-	if newEnvironment is EnvironmentSettings:
-		currentEnvironmentRef = newEnvironment
-		#print("player - entered newEnvironment: ", newEnvironment)
-
-func exitedEnvironment(environment):
-	if environment == currentEnvironmentRef:
-		currentEnvironmentRef == null
-		#print("player - exited the environment: ", environment)
-
-
-
-
-func checkIfAnimationShouldStop():
-	print("player - checking if dead: ", %PlayerSprite.animation)
-	if %PlayerSprite.animation == "Dieing":
-		print("player - player is dead: ")
-		%PlayerSprite.stop()
-
-
-
-
+	%Flashlight.visible = false
 
 #
